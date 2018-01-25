0712c58d07787f7a0295e3a76d35133b5bcef55b
ktennant <keith.tennant@healthstream.com>
2018-01-25 10:06:14 -0600

    leaderboard edits

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,16 +1,8 @@
 <template>
     <section class="section">
         <div class="container">
-            <h1 class="title">Summary</h1>
-            <b-table
-                :data="isEmpty ? [] : tableData"
-                :bordered="isBordered"
-                :striped="isStriped"
-                :narrowed="isNarrowed"
-                :hoverable="isHoverable"
-                :loading="isLoading"
-                :mobile-cards="hasMobileCards">
-
+            <h1 class="title">Leaderboard</h1>
+            <b-table striped mobile-cards default-sort="percent_change" default-sort-direction="desc" :data="tableData">
                 <template slot-scope="props">
                     <b-table-column label="Name">
                         <span>{{ props.row.name }}</span>
@@ -67,8 +59,23 @@
                         <span v-if="props.row.week12==undefined">--</span>
                         <span v-else>{{ props.row.week12 }}</span>
                     </b-table-column>
-                    <b-table-column label="Total Loss">
-                        <span>{{ props.row.percentageWeightLoss }} | {{ props.row.totalWeightLoss }}</span>
+                    <b-table-column label="Total Loss" field="total_change" sortable>
+                        <span v-if="props.row.total_change < 0" class="success">
+                            {{ props.row.total_change }} lbs
+                        </span>
+                        <span v-else>
+                            +{{ props.row.total_change }} lbs
+                        </span>                        
+                    </b-table-column>
+                    <b-table-column label="Percent Change" field="percent_change" sortable>
+                        <span v-if="props.row.total_change < 0" class="success">
+                            {{ props.row.percent_change }}%
+                            <b-icon icon="arrow-down-thick" size="is-small" type="is-success"></b-icon>
+                        </span>
+                        <span v-else>
+                            {{ props.row.percent_change }}%
+                            <b-icon icon="arrow-up-thick" size="is-small" type="is-danger"></b-icon>
+                        </span>                        
                     </b-table-column>
                 </template>
             </b-table>
@@ -88,24 +95,15 @@
             contestant['week'+i] = p.weighins[i];
             currentWeek = (p.weighins[i] != undefined) ? i : currentWeek;
         };
-
-        contestant.totalWeightLoss = p.weighins[0] - p.weighins[currentWeek];
-        contestant.percentageWeightLoss = Math.round((p.weighins[currentWeek] / p.weighins[0]) * 100) / 100;
-
+        contestant.total_change = Math.round((p.weighins[currentWeek] - p.weighins[0]) * 10) / 10;
+        contestant.percent_change = Math.abs(Math.round((((p.weighins[currentWeek] / p.weighins[0]) * 100) - 100) * 100) / 100);
         tableData.push(contestant);
     });    
-
+    console.log(tableData)
     export default {
         data() {
             return {
-                tableData,
-                isEmpty: false,
-                isBordered: false,
-                isStriped: true,
-                isNarrowed: false,
-                isHoverable: false,
-                isLoading: false,
-                hasMobileCards: true
+                tableData
             }
         }
     }
